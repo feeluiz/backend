4a01f8058d940c6c79a3516a3c6d11154dfbef8a
feeluiz <44549332+feeluiz@users.noreply.github.com>
2019-10-16 15:04:15 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const detailsController = require("./controllers/detailsController")
 
 var routes = express.Router
 
-routes.post("/users" , (req,res)=> res.json({'message':'Bem Vindo'}))
+routes.get("/" , (req,res)=> res.json({'message':'Bem Vindo'}))
             
 routes.post("/users" , usersController.search)
 routes.post("/details" , detailsController.getDetails)
