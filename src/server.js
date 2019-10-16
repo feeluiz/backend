66b5a1e41052d97fb7710104a46dcc47bf279bf5
Fernando Luiz <feluiz95@gmail.com>
2019-10-16 16:27:14 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,15 +1,12 @@
 const express = require("express")
 const usersController = require("./controllers/usersController")
 const detailsController = require("./controllers/detailsController")
+const cors = require('cors')
 //const routers = require('./routes')
 
 const server = express()
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+server.use(cors())
 server.use(express.json())
 
 server.get("/" , (req,res)=> res.json({'message':'Bem Vindo'}))
